32fa2bad580cd8160fb91ec4520550930c7d2f49
Ahmad <ahmad.alhourani@xlabtechs.com>
2019-03-25 16:46:17 +0200

    Company

---
+++ database/migrations/2019_03_25_1646071_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name1134341');
+            $table->string('sms')->nullable();
+
+            $table->timestamps();
+            $table->string('slug');
+            $table->softDeletes();
+            $table->unique('slug');
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ resources/views/backend/companies/index.blade.php
@@ -0,0 +1,46 @@
+@extends('backend.layouts.app')
+
+@section('title', app_name() . ' | ' . __('strings.backend.companies.title'))
+
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-sm-5">
+                    <h4 class="card-title mb-0">
+                        {{ __('labels.backend.companies.management') }}
+                    </h4>
+                </div><!--col-->
+
+                <div class="col-sm-4">
+                    <form name="condition" action="" method="get">
+                    {{--   <input type="text" name="search" placeholder="search">--}}
+                    </form>
+                </div><!--col-->
+
+                <div class="col-sm-3 pull-right">
+                    @include('backend.companies.includes.header-buttons')
+                </div><!--col-->
+            </div><!--row-->
+
+            <div class="row mt-4">
+                <div class="col">
+                    @include('backend.companies.table')
+                </div><!--col-->
+            </div><!--row-->
+            <div class="row">
+                <div class="col-7">
+                    <div class="float-left">
+                        @if(isset($part)){{ $part}} from  @endif  {!! $companies->total() !!} {{ trans_choice('labels.backend.companies.table.total', $companies->total()) }}
+                    </div>
+                </div><!--col-->
+                <div class="col-5">
+                    <div class="float-right">
+                        {!! $companies->render() !!}
+                    </div>
+                </div><!--col-->
+            </div><!--row-->
+        </div><!--card-body-->
+    </div><!--card-->
+@endsection
